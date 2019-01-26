744027fe34699d2856d76799988884e511a7ef65
Gustavo Reis <gusbemacbe@gmail.com>
2019-01-26 06:03:28 -0200

    Beta 17.2

---
+++ scripts/_fix_color_scheme.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+#
+# Written in 2016 by Sergei Eremenko <https://github.com/SmartFinn>
+#
+# To the extent possible under law, the author(s) have dedicated all copyright
+# and related and neighboring rights to this software to the public domain
+# worldwide. This software is distributed without any warranty.
+#
+# You should have received a copy of the CC0 Public Domain Dedication along
+# with this software. If not, see
+# <http://creativecommons.org/publicdomain/zero/1.0/>.
+#
+# Description:
+#  This script looks in the SVG files for certain colors and replaces
+#  them with the corresponding stylesheet class. Fixes a color scheme
+#  after Inkscape.
+#
+# Limitations:
+#  - works only with one element per line
+#
+# Usage:
+#  _fix_color_scheme.sh FILE...
+
+set -e
+
+# Suru++
+add_class() {
+	# add the class if a value matches:
+	sed -i -r \
+		-e '/([^-]color|fill|stop-color|stroke):(#808080|#babdb6|#f7f7f7|#ffffff)/I s/(style="[^"]+")/\1 class="ColorScheme-Text"/' \
+		-e '/([^-]color|fill|stop-color|stroke):(#5294e2|#3daee9|#1d99f3)/I s/(style="[^"]+")/\1 class="ColorScheme-Highlight"/' \
+		-e '/([^-]color|fill|stop-color|stroke):#27ae60/I s/(style="[^"]+")/\1 class="ColorScheme-PositiveText"/' \
+		-e '/([^-]color|fill|stop-color|stroke):#da4453/I s/(style="[^"]+")/\1 class="ColorScheme-NegativeText"/' \
+		-e '/([^-]color|fill|stop-color|stroke):#f67400/I s/(style="[^"]+")/\1 class="ColorScheme-NeutralText"/' \
+		-e '/([^-]color|fill|stop-color|stroke):#4f575f/I s/(style="[^"]+")/\1 class="ColorScheme-ViewBackground"/' \
+		"$@"
+}
+
+replace_hex_to_current_color() {
+	# if class exist:
+	#   - remove color
+	#   - replace fill=#HEXHEX to fill=currentColor
+	sed -i -r \
+		-e '/class="ColorScheme-/ s/([^-])color:#([[:xdigit:]]{3}|[[:xdigit:]]{6});?/\1/' \
+		-e '/class="ColorScheme-/ s/fill:#([[:xdigit:]]{3}|[[:xdigit:]]{6});?/fill:currentColor;/' \
+		-e '/class="ColorScheme-/ s/stroke:#([[:xdigit:]]{3}|[[:xdigit:]]{6});?/stroke:currentColor;/' \
+		-e '/class="ColorScheme-/ s/stop-color:#([[:xdigit:]]{3}|[[:xdigit:]]{6});?/stop-color:currentColor;/' \
+		"$@"
+}
+
+for file in "$@"; do
+	# continue if it's a file
+	[ -f "$file" ] || continue
+
+	if grep -q -i 'id="current-color-scheme"' "$file"; then
+		# the file has a color scheme
+
+		replace_hex_to_current_color "$file"
+
+		add_class "$file"
+
+		replace_hex_to_current_color "$file"
+	fi
+done
---
+++ scripts/build_color_folders.sh
@@ -0,0 +1,170 @@
+#!/usr/bin/env bash
+# This script creates colored folder icons for Suru+ icon theme
+#
+# Colors of the folder icon:
+#
+#   @ - primary color
+#	" - secondary color
+#	. - color of symbol
+#
+#     """""""""""""""""
+#    """"""""""""""""""""
+#    """"""""""""""""""""""""""""""""""""""""
+#    """"""""""""""""""""""""""""""""""""""""
+#    """"""""""""""""""""@@@@@@@@@@@@@@@@@@@"
+#    """"""""""""""""""@@@@@@@@@@@@@@@@@@@@@@
+#    .@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+#    @@@@@@@@@@@@@@@@@@....@@@@@@@@@@@@@@@@@@
+#    @@@@@@@@@@@@@@@@@@.@@.@@@@@@@@@@@@@@@@@@
+#    @@@@@@@@@@@@@@@@@@.@@.@@@@@@@@@@@@@@@@@@
+#    @@@@@@@@@@@@@@@@@@.@@.@@@@@@@@@@@@@@@@@@
+#    @@@@@@@@@@@@@......@@......@@@@@@@@@@@@@
+#    @@@@@@@@@@@@@@.@@@@@@@@@@.@@@@@@@@@@@@@@
+#    @@@@@@@@@@@@@@@.@@@@@@@@.@@@@@@@@@@@@@@@
+#    @@@@@@@@@@@@@@@@@..@@..@@@@@@@@@@@@@@@@@
+#    @@@@@@@@@@@@@@@@@@@..@@@@@@@@@@@@@@@@@@@
+#    @@@@@@@@@@@@@@............@@@@@@@@@@@@@@
+#    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+#    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+set -eo pipefail
+
+THIS_SCRIPT="$(basename "$0")"
+
+if [ -d "$1" ]; then
+	TARGET_DIR="$1"
+else
+	printf "%s\n" "usage: $THIS_SCRIPT <path to icon theme>" >&2
+	exit 128
+fi
+
+DEFAULT_COLOR="blue"
+SIZES_REGEX="(16|scalable)"
+MONOCHROME_SIZES_REGEX="(16)"
+COLOR_SIZES_REGEX="(scalable)"
+FILES_REGEX="(folder|user)-"
+
+declare -A COLORS
+
+COLORS=(
+	# [0] - primary color
+	# [1] - secondary color
+	# [2] - color of symbol
+	# [3] - color of panel
+	#
+	# | name   | [0]   | [1]   | [2]   | [3]   |
+	# |--------|-------|-------|-------|-------|
+	[blue]="    #37A6E6 #1463A5 #FFFFFF #333333"
+	[black]="   #1A1A1A #333333 #FFFFFF #000000"
+	[bluegrey]="#78909C #455A64 #CFD8DC #333333"
+	[brown]="   #AE8E6C #957552 #FFFFFF #333333"
+	[cyan]="    #31C6B7 #2B898F #FFFFFF #333333"
+	[green]="   #76C22B #28851E #FFFFFF #333333"
+	[grey]="    #999999 #666666 #FFFFFF #333333"
+	[magenta]=" #F74761 #C61B34 #FFFFFF #333333"
+	[orange]="  #fb7c38 #f34f17 #FFFFFF #333333"
+	[red]="     #F22C42 #BC1938 #FFFFFF #333333"
+	[teal]="    #26A69A #00796B #FFFFFF #333333"
+	[violet]="  #8F76E4 #7839B7 #FFFFFF #333333"
+	[yellow]="  #fdc92b #eab305 #333333 #333333"
+	[custom]="  #value_light #value_dark #323232 #e4e4e4"
+)
+
+
+msg() {
+	printf "%b => %b%s\n" "\e[1;32m" "\e[0m" "$*" >&2
+}
+
+warn() {
+	printf "%b [+] %b%s\n" "\e[1;33m" "\e[0m" "$*" >&2
+}
+
+recolor() {
+	# args: <old colors> <new colors> <path to file>
+	declare -a old_colors=( $1 )
+	declare -a new_colors=( $2 )
+	local filepath="$3"
+
+	[ -f "$filepath" ] || exit 1
+
+	for (( i = 0; i < "${#old_colors[@]}"; i++ )); do
+		sed -i "s/${old_colors[$i]}/${new_colors[$i]}/gI" "$filepath"
+	done
+}
+
+
+msg "PHASE 1: Delete color suffix from monochrome icons ..."
+# -----------------------------------------------------------------------------
+find "$TARGET_DIR" -regextype posix-extended \
+	-regex ".*/places/${MONOCHROME_SIZES_REGEX}/${FILES_REGEX}${DEFAULT_COLOR}-.*" \
+	-print0 | while read -r -d $'\0' file; do
+
+	new_file="${file/-$DEFAULT_COLOR-/-}"
+
+	warn "'$file' is renamed to '$new_file'"
+	mv -f "$file" "$new_file"
+done
+
+
+msg "PHASE 2: Create missing symlinks ..."
+# -----------------------------------------------------------------------------
+find "$TARGET_DIR" -type f -regextype posix-extended \
+	-regex ".*/places/${COLOR_SIZES_REGEX}/${FILES_REGEX}${DEFAULT_COLOR}[-\.].*" \
+	-print0 | while read -r -d $'\0' file; do
+
+	target="$(basename "$file")"
+	symlink="${file/-$DEFAULT_COLOR/}"
+
+	[ -e "$symlink" ] && continue
+
+	warn "Creating missing symlink '$symlink' ..."
+	ln -sf "$target" "$symlink"
+done
+
+
+msg "PHASE 3: Generate color folders ..."
+# -----------------------------------------------------------------------------
+find "$TARGET_DIR" -type f -regextype posix-extended \
+	-regex ".*/places/${SIZES_REGEX}/${FILES_REGEX}${DEFAULT_COLOR}[-\.].*" \
+	-print0 | while read -r -d $'\0' file; do
+
+	for color in "${!COLORS[@]}"; do
+		[[ "$color" != "$DEFAULT_COLOR" ]] || continue
+
+		new_file="${file/-$DEFAULT_COLOR/-$color}"
+
+		cp -P --remove-destination "$file" "$new_file"
+		recolor "${COLORS[$DEFAULT_COLOR]}" "${COLORS[$color]}" "$new_file"
+	done
+done
+
+
+msg "PHASE 4: Create symlinks for Folder Color v0.0.80 and newer ..."
+# -----------------------------------------------------------------------------
+# Icons mapping
+FOLDER_COLOR_MAP=(
+	# Folder Color icon         | Suru-plus icon
+	# --------------------------|---------------------------
+	"folder-COLOR-desktop.svg    user-COLOR-desktop.svg"
+	"folder-COLOR-downloads.svg  folder-COLOR-download.svg"
+	"folder-COLOR-public.svg     folder-COLOR-image-people.svg"
+	"folder-COLOR-videos.svg     folder-COLOR-video.svg"
+)
+
+for mask in "${FOLDER_COLOR_MAP[@]}"; do
+	for color in "${!COLORS[@]}"; do
+		icon_mask=( $mask )
+		folder_color_icon="${icon_mask[0]/COLOR/$color}"
+		icon="${icon_mask[1]/COLOR/$color}"
+
+		find "$TARGET_DIR" -regextype posix-extended \
+			-regex ".*/places/${SIZES_REGEX}/${icon}" \
+			-print0 | while read -r -d $'\0' file; do
+
+			base_name="$(basename "$file")"
+			dir_name="$(dirname "$file")"
+
+			ln -sf "$base_name" "$dir_name/$folder_color_icon"
+		done
+	done
+done
