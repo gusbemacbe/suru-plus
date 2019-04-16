9607ddd43479ebeb3019576f9b3559077594407f
Gustavo Reis <gusbemacbe@gmail.com>
2019-04-16 05:45:23 -0300

    Small updates for the folder 'tools'

--- scripts/script3.sh
+++
@@ -1,8 +0,0 @@
-# find .  -type l -lname 'accessibility.svg' -printf 'ln -nsf yyy "%p"\n' > script2.sh
-
-echo "Nuovo collegamento"
-read nuovo
-
-for vecchio in *.svg; do
-   ln -sfnr $nuovo $vecchio 
-done
